83ecea2b2f9530889994bc9c5c132163506e985e
zhangyangshijia1 <zhangyangshijia@gmail.com>
2025-03-20 23:16:23 +0800

    Site updated: 2025-03-20 23:16:20

---
+++ anzhiyu/random.js
@@ -0,0 +1,3 @@
+var posts=["smallhao/Suite使用介绍（一）/","smallhao/MySQL注入技巧/","smallhao/内网渗透-免杀/","smallhao/平台搭建-DNSLog/","smallhao/信息收集-敏感信息收集/"];function toRandomPost(){
+    pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
+  };
